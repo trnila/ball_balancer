6a49bee2b687fc9545b7d5052d7ba79982b7214a
Daniel Trnka <daniel.trnka@gmail.com>
2018-03-22 20:02:44 +0100

    ADC: using DMA

--- Src/stm32f1xx_it.c
+++ Src/stm32f1xx_it.c
@@ -37,10 +37,11 @@
 #include "cmsis_os.h"
 
 /* USER CODE BEGIN 0 */
-
+BaseType_t pxHigherPriorityTaskWoken;
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
+extern DMA_HandleTypeDef hdma_adc1;
 extern TIM_HandleTypeDef htim3;
 extern DMA_HandleTypeDef hdma_usart1_tx;
 extern UART_HandleTypeDef huart1;
@@ -160,6 +161,21 @@ void SysTick_Handler(void)
 /* For the available peripheral interrupt handler names,                      */
 /* please refer to the startup file (startup_stm32f1xx.s).                    */
 /******************************************************************************/
+
+/**
+* @brief This function handles DMA1 channel1 global interrupt.
+*/
+void DMA1_Channel1_IRQHandler(void)
+{
+  /* USER CODE BEGIN DMA1_Channel1_IRQn 0 */
+
+  /* USER CODE END DMA1_Channel1_IRQn 0 */
+  HAL_DMA_IRQHandler(&hdma_adc1);
+  /* USER CODE BEGIN DMA1_Channel1_IRQn 1 */
+  portEND_SWITCHING_ISR(pxHigherPriorityTaskWoken);
+  pxHigherPriorityTaskWoken = 0;
+  /* USER CODE END DMA1_Channel1_IRQn 1 */
+}
 
 /**
 * @brief This function handles DMA1 channel4 global interrupt.
