88ddd7aaa6c4d85da210406c403d06b10b96729f
Daniel Trnka <daniel.trnka@gmail.com>
2018-03-24 21:02:48 +0100

    new pid constants

--- Inc/balancer/configuration.h
+++ Inc/balancer/configuration.h
@@ -4,8 +4,17 @@ struct Configuration {
 	double const_p, const_d, const_i;
 
 	Configuration() {
-		const_p = 0.00002;
+/*
+  		const_p = 0.00001;
 		const_i = 0.00002;
-		const_d = 0.00001;
+		const_d = 0.000006;
+
+		const_p = 0.00001;
+		const_i = 0.000005;
+		const_d = 0.000006;
+ */
+		const_p = 0.000005;
+		const_i = 0.000005;
+		const_d = 0.000006;
 	}
 };
