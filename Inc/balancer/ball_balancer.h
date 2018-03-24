e120c0b90e2a7768d3bb84ae48e8508e1cc5ce5c
Daniel Trnka <daniel.trnka@gmail.com>
2018-03-24 19:12:53 +0100

    removed unused control period

--- Inc/balancer/ball_balancer.h
+++ Inc/balancer/ball_balancer.h
@@ -18,10 +18,7 @@ struct __attribute__((__packed__))  Measurement {
 
 class ball_balancer {
 public:
-	ball_balancer(
-			VelocityTracker &tracker,
-			Configuration &conf
-	): tracker(tracker), conf(conf), controlPeriodMs(CONTROL_PERIOD_MS) {
+	ball_balancer(VelocityTracker &tracker, Configuration &conf): tracker(tracker), conf(conf) {
 		reset();
 	}
 
@@ -30,66 +27,45 @@ public:
 	}
 
 	bool update(Measurement &meas) {
-		bool updated = tracker.update();
+		bool touchDetected = tracker.update();
 
 		Vectorf target(SIZE_X / 2, SIZE_Y / 2);
 		Vectorf speed = tracker.getSpeed();
 		Vectorf accel = tracker.getAcceleration();
 		Vectorf change = conf.const_p * speed + conf.const_d * accel + conf.const_i * (tracker.getPos() - target);
 
-		if(updated) {
+		if(touchDetected) {
 			planeNormal.x = cap(planeNormal.x - change.x);
 			planeNormal.y = cap(planeNormal.y - change.y);
 			planeNormal = normalize(planeNormal);
 		}
 
+		double zx = planeNormal.x * MX / planeNormal.z;
+		double zy = planeNormal.y * MY / planeNormal.z;
 
-		//bool controlled = controlPeriodMs == 0 || (controlPeriodMs && i % controlPeriodMs == 0);
-		//if(controlled) {
-			double zx = planeNormal.x * MX / planeNormal.z;
-			double zy = planeNormal.y * MY / planeNormal.z;
+		double angleX = zx / PX;
+		double angleY = zy / PY;
 
-			double angleX = zx / PX;
-			double angleY = zy / PY;
+		int USX = CENTER_X_US + angleX / 0.5f * 600;
+		int USY = CENTER_Y_US + angleY / 0.5f * 600;
 
-			/*
-			double DX = angleX * 180 / M_PI;
-			double DY = angleY * 180  / M_PI;
-*/
-			int USX = CENTER_X_US + angleX / 0.5f * 600;
-			int USY = CENTER_Y_US + angleY / 0.5f * 600;
+		meas.magic[0] = 0xAB;
+		meas.magic[1] = 0xCD;
+		meas.cx = change.x; meas.cy = change.y;
+		meas.vx = speed.x; meas.vy = speed.y;
+		meas.posx = (float) tracker.getPos().x; meas.posy = (float) tracker.getPos().y;
+		meas.rvx = tracker.getSpeed().x; meas.rvy = tracker.getSpeed().y;
+		meas.rax = tracker.getAcceleration().x; meas.ray = tracker.getAcceleration().y;
+		meas.nx = planeNormal.x; meas.ny = planeNormal.y;
+		meas.RX = (float) tracker.getResistance().x; meas.RY = (float) tracker.getResistance().y;
+		meas.USX = USX; meas.USY = USY;
+		meas.rawx = tracker.getRawResistance().x;
+		meas.rawy = tracker.getRawResistance().y;
 
-
-
-
-
-			meas.magic[0] = 0xAB;
-			meas.magic[1] = 0xCD;
-			meas.cx = change.x; meas.cy = change.y;
-			meas.vx = speed.x; meas.vy = speed.y;
-			meas.posx = (float) tracker.getPos().x; meas.posy = (float) tracker.getPos().y;
-			meas.rvx = tracker.getSpeed().x; meas.rvy = tracker.getSpeed().y;
-			meas.rax = tracker.getAcceleration().x; meas.ray = tracker.getAcceleration().y;
-			meas.nx = planeNormal.x; meas.ny = planeNormal.y;
-			meas.RX = (float) tracker.getResistance().x; meas.RY = (float) tracker.getResistance().y;
-			meas.USX = USX; meas.USY = USY;
-			meas.rawx = tracker.getRawResistance().x;
-			meas.rawy = tracker.getRawResistance().y;
-		//}
-
-		i++;
-
-		return true;
-	}
-
-	void setControlPeriod(int ms) {
-		controlPeriodMs = ms;
+		return touchDetected;
 	}
 
 private:
-	int i;
-	int controlPeriodMs;
-
 	VelocityTracker &tracker;
 	Configuration &conf;
 
--- Inc/balancer/config.h
+++ Inc/balancer/config.h
@@ -1,22 +1,16 @@
 #pragma once
 
-#define STM32
-
 const int MX = 85, MY = 85; // montazni bod kloubu na ose x, y
 const int PX = 29, PY = 29; // delka paky na servu na ose x, y
 const int CENTER_X_US = 1500;
 const int CENTER_Y_US = 1500;
 const double SHIFT_MM = 14;
-const int DUTY_MS = 20;
+const int MEASUREMENT_PERIOD_MS = 20;
 // 30-16mm
 
 const int SIZE_Y = 230;
 const int SIZE_X = 170;
 
-const int MEASUREMENT_PERIOD_MS = 20;
-const int CONTROL_PERIOD_MS = DUTY_MS / MEASUREMENT_PERIOD_MS;
-
-const int MAX_CMD_ARGS = 6;
 
 //const int RminY = 10600, RmaxY = 44900;
 //const int RminX = 8130, RmaxX = 50700;
